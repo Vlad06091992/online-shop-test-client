e458a84a6f4e8891b711767b762456b3af588b3e
Vlad <Smirnov.ru92@mail.ru>
2023-12-02 20:24:39 +0500

    upd URL

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const instance = axios.create({
-    baseURL: 'https://online-shop-test-server-dcrd-d149oteic-vlad06091992.vercel.app/',
+    baseURL: 'https://online-shop-test-server.vercel.app/',
     withCredentials:true,
 })
 
