a5be23044c944989df9526cf7cd6fc04226535e3
Vlad <Smirnov.ru92@mail.ru>
2023-12-02 19:27:00 +0500

    fixed cors

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,3 +3,12 @@ export const instance = axios.create({
     baseURL: 'https://online-shop-test-server-dcrd-d149oteic-vlad06091992.vercel.app/',
     withCredentials:true,
 })
+
+// Добавляем интерсептор для обработки предварительных запросов
+instance.interceptors.request.use(config => {
+    // Устанавливаем заголовки, необходимые для предварительных запросов
+    config.headers['Access-Control-Request-Method'] = 'GET'; // Укажите метод, который ваш сервер поддерживает
+    config.headers['Access-Control-Request-Headers'] = 'Content-Type, Authorization'; // Укажите необходимые заголовки
+
+    return config;
+});
